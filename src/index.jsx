d73dde6eeef451121e47539e822fa4139fe8c0be
Arantxapancha <arantxamm349@gmail.com>
2024-01-19 14:02:19 +0100

    Initial Commit

---
+++ src/index.jsx
@@ -0,0 +1,19 @@
+// import React from "react";
+// import  ReactDOM  from "react-dom";
+
+// ReactDOM.render(
+//     <div>Hola mundo!!!</div>,
+//     document.getElementById("root")
+// );
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const container = document.getElementById('root');
+
+// root es la raíz de la aplicación que se monta en el div#root
+const root = createRoot(container);
+
+// renderiza algo en la raíz de la aplicación
+root.render(<React.StrictMode><App /></React.StrictMode>);
